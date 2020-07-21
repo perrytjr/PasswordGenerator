5eb2fd7a63763db3f892c2dc24c6a9bb8f0e609a
Perry Theobald <perrytjr@gmail.com>
2020-07-21 13:08:30 -0700

    updated readme and updated javascript

--- Develop/script.js
+++ Develop/script.js
@@ -3,60 +3,99 @@ var generateBtn = document.querySelector("#generate");
 
 
 //Arrays
-const pNumeric = '1234567890'.split('');
-const lowerCase = 'abcdefghijklmnopqrstuvwxyz'.split('');
-const upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
-const specialCharacters = '!#$%&()*+,-./:;<=>?@[]^_{|}~'.split('');
-
-
-//variables
-
-
-
-
+const pNumeric = '1234567890'
+const lowerCase = 'abcdefghijklmnopqrstuvwxyz'
+const upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+const specialCharacters = '!#$%&()*+,-./:;<=>?@[]^_{|}~'
+//decalred variables 
+var pLength;
+var lCase;
+var upCase;
+var pNum;
+var sCharacters;
 
 function writePassword() {
+  var prompts = getprompts();
   var password = generatePassword();
+
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
 }
+//this function controls the promts, confirms, if and do while loop
+function getprompts() {
+  // user picks length of password
+  pLength = parseInt(prompt('Please pick a number between 8 and 128. This will be the length of your password!'));
 
-//passwordlength is obtained from user
-function generatePassword(){
- 
-// user picks length of password
-let pLength = parseInt(prompt('Please pick length of password between 8 and 128 characters!'));
+  // if user picks length of password less than 8 or greater that 128 or not a number user is asked to pick between 8 and 128
+  if (pLength < 8 || pLength > 128 || isNaN(pLength)) {
+    pLength = prompt('Remember password length is any number between 8 and 128. Please choose again.');
+  }
 
-// if user picks length of password less than 8 or greater that 128 user is asked to pick between 8 and 128
-while(pLength < 8 || pLength > 128 || isNaN(pLength)) {
-  pLength = prompt('Remember password is between 8 and 128 characters long. Please choose again.');
+  
+  console.log('Password Length is ' + pLength);
+
+  //do all the user confirms bellow while they are all false. 
+  do {
+    console.log('whilecheck')
+    lCase = confirm('Do you want to add lowercase letters to your password?');
+    console.log('picked lowercase' + lCase);
+
+    upCase = confirm('Do you want to add uppercase letters to your password?');
+    console.log('picked uppercase' + upCase);
+
+    pNum = confirm('Do you want to add numbers to your password?');
+    console.log('picked numeric' + pNum);
+
+    sCharacters = confirm('Do you want to add special characters to your password?');
+    console.log('picked specialcharacters' + sCharacters);
+    //this alerts the user to pick at least one criteria options for their password
+    if (!lCase && !upCase && !pNum && !sCharacters){
+    alert('You have to pick at least one of the previouse criteria options for your password');
+    }
+  }
+  while (!lCase && !upCase && !pNum && !sCharacters);
 }
+//This function creates the password. It organizes the users criteria input 
+function generatePassword() {
+  //variable to store user category choices for password randomization
+  let allowed = "";
 
-console.log('Password Length is ' + pLength);
+  if (lCase) {
+    allowed = allowed + lowerCase;
+  }
 
+  if (upCase) {
+  
+    allowed = allowed + upperCase;
+  }
 
+  if (pNum) {
+    
+    allowed = allowed + pNumeric;
+  }
+
+  if (sCharacters) {
+
+    allowed = allowed + specialCharacters;
+  }
+  console.log("allowed characters" + allowed);
+  //making length equal to the length the user picked for password
+  var length = parseInt(pLength);
+  //newPassword will be created bellow in the for. 
+  var newPassword ="";
+// this for takes the category choices of the user that are stored in the allowed is asigned the length from the user input is radomnized by the math random and is combined into newPassword
+  for (let i = 0; i < pLength; i++) {
+    var random = Math.floor(Math.random() * allowed.length);
+    newPassword += allowed[random];
+  }
+
+  console.log(newPassword)
+//this returns the password to be printed on the screen!!!
+  return newPassword;
 }
-
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-
-
-/* GIVEN I need a new, secure password
-WHEN I click the button to generate a password
-THEN I am presented with a series of prompts for password criteria
-WHEN prompted for password criteria
-THEN I select which criteria to include in the password
-WHEN prompted for the length of the password
-THEN I choose a length of at least 8 characters and no more than 128 characters
-WHEN prompted for character types to include in the password
-THEN I choose lowercase, uppercase, numeric, and/or special characters
-WHEN I answer each prompt
-THEN my input should be validated and at least one character type should be selected
-WHEN all prompts are answered
-THEN a password is generated that matches the selected criteria
-WHEN the password is generated
-THEN the password is either displayed in an alert or written to the page */
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# PasswordGenerator
+# Password Generator
+
+## Overview 
+
+
+![](Assets/Screen Shot 2020-07-21 at 9.13.25 AM.png)
+
+I created an application that generates a random password based on user-selected criteria. This app will run in the browser using HTML, CSS, powered by JavaScript. 
+
+## How it functions
+
+When a user clicks on the generate password button. A series of prompts will appear asking for user criteria that he or she would like to use in their password. The password must be between 8-128 characters long. It must include one character type and if the user doesnt pick one, they will be reminded to do so. The password generator will display a randomized password in the box above the generate password button. 
+
+
+
+# Link
+
+Please use the following link to access my Password Generator site: https://github.com/perrytjr/PasswordGenerator
+
